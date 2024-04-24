81d375013bbbd84037cb0fa3b23c0867f0c10dad
jsongalvez <jsgalvez@mymail.mapua.edu.ph>
2024-04-24 21:03:49 +0800

    menu

---
+++ menu/oled.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.lcd.device import st7735
+
+def main():
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((40,40), "Hello World", fill="red")
+    while True:
+        pass
+    
+
+if __name__ == "__main__":
+    try:
+        serial = spi(port=0, device=0, gpio_DC=23, gpio_RST=24)
+        device = st7735(serial, rotate=0)
+        main()
+    except KeyboardInterrupt:
+        pass
