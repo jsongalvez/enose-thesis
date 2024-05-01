a950a48e8877f923226801376f4683e1ea3e89ae
jsongalvez <jsgalvez@mymail.mapua.edu.ph>
2024-05-01 16:39:27 +0800

    changed filenames

--- rotary_encoder/rotary.py
+++ rotary_encoder/rotary.py
@@ -1,79 +1,88 @@
-#import machine
-#import utime as time
-#from machine import Pin
-#import micropython
 import OPi.GPIO as GPIO
-import atexit
 
-atexit.register(GPIO.cleanup)
+#  #board pin (physical)
+#  dt_pin_rise = 22
+#  dt_pin_fall = 29
+#  clk_pin_rise = 37
+#  clk_pin_fall = 31
+#  sw_pin = 26
+#  
+#  #default values
+#  val_dt = 1
+#  val_clk = 1
+#  val_sw = 0
+#  val_dt_prev = val_dt
+#  val_clk_prev = val_clk
+#  val_sw_prev = val_sw
+#  
+#  #channels (GPIO)
+#  CH_DT_RISE = 20
+#  CH_DT_FALL = 8
+#  CH_CLK_RISE = 2
+#  CH_CLK_FALL = 7
+#  CH_SW = 21
 
 class Rotary:
-    
-    ROT_CW = 1
-    ROT_CCW = 2
-    SW_PRESS = 4
-    SW_RELEASE = 8
-    
-    def __init__(self,dt,clk,sw):
-        print("__init__")
-        #self.dt_pin = Pin(dt, Pin.IN)
-        #self.clk_pin = Pin(clk, Pin.IN)
-        #self.sw_pin = Pin(sw, Pin.IN)
-        self.dt_pin = dt
-        self.clk_pin = clk
-        self.sw_pin = sw
-        GPIO.setboard(4)
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.dt_pin, GPIO.IN)
-        GPIO.setup(self.clk_pin, GPIO.IN)
-        GPIO.setup(self.sw_pin, GPIO.IN)
-        self.last_status = (GPIO.input(self.dt_pin) << 1) | GPIO.input(self.clk_pin)
-        #self.dt_pin.irq(handler=self.rotary_change, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING )
-        #self.clk_pin.irq(handler=self.rotary_change, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING )
-        #self.sw_pin.irq(handler=self.switch_detect, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING )
-        GPIO.add_event_detect(self.dt_pin, GPIO.BOTH, callback=self.rotary_change)
-        GPIO.add_event_detect(self.clk_pin, GPIO.BOTH, callback=self.rotary_change)
-        GPIO.add_event_detect(self.sw_pin, GPIO.BOTH, callback=self.switch_detect)
-        self.handlers = []
-        self.last_button_status = GPIO.input(self.sw_pin)
 
-    def __exit__(self):
-        print("__exit__")
+    def __init__(self, dt1, dt2, clk1, clk2, sw):
+        self.dt_pin_rise = dt1
+        self.dt_pin_fall = dt2
+        self.clk_pin_rise = clk1
+        self.clk_pin_fall = clk2
+        self.sw_pin = sw
+
+        GPIO.setboard(4) # Orange Pi One
+        GPIO.setmode(GPIO.SOC) # GPIO column in gpio readall
+        
+        GPIO.setup(dt_pin_rise, GPIO.IN)
+        GPIO.setup(dt_pin_fall, GPIO.IN)
+        GPIO.setup(clk_pin_rise, GPIO.IN)
+        GPIO.setup(clk_pin_fall, GPIO.IN)
+        GPIO.setup(sw_pin, GPIO.IN)
+        
+        GPIO.add_event_detect(self.dt_pin_rise, GPIO.RISING, callback=self.rotary_change, bouncetime=4)
+        GPIO.add_event_detect(self.dt_pin_fall, GPIO.FALLING, callback=self.rotary_change, bouncetime=4)
+        GPIO.add_event_detect(self.clk_pin_rise, GPIO.RISING, callback=self.rotary_change, bouncetime=4)
+        GPIO.add_event_detect(self.clk_pin_fall, GPIO.FALLING, callback=self.rotary_change, bouncetime=4)
+        GPIO.add_event_detect(self.sw_pin, GPIO.RISING, callback=sw_change)
+
+        self.prev_status = 0b11
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
         GPIO.cleanup()
 
     def rotary_change(self, pin):
-        print(f"dt = {GPIO.input(self.dt_pin)} clk = {GPIO.input(self.clk_pin)} sw = {GPIO.input(self.sw_pin)}")
-        new_status = (GPIO.input(self.dt_pin) << 1) | GPIO.input(self.clk_pin)
-        if new_status == self.last_status:
-            return
-        transition = (self.last_status << 2) | new_status
-        if transition == 0b1110:
-            #micropython.schedule(self.call_handlers, Rotary.ROT_CW)
-            self.call_handlers(Rotary.ROT_CW)
-        elif transition == 0b1101:
-            #micropython.schedule(self.call_handlers, Rotary.ROT_CCW)
-            self.call_handlers(Rotary.ROT_CCW)
-        self.last_status = new_status
+        # Which edge fell
+        if pin == self.dt_pin_fall:
+            curr_dt = 0
+        elif pin == self.clk_pin_fall:
+            curr_clk = 0
+        elif pin == self.dt_pin_rise:
+            curr_dt = 1
+        elif pin == self.clk_pin_rise:
+            curr_clk = 1
         
-    def switch_detect(self,pin):
-        if self.last_button_status == GPIO.input(self.sw_pin):
+        # Find way to offload below as scheduled task or something
+
+        # 2 bits
+        curr_status = curr_dt << 1 | curr_clk
+
+        # ignore repeats
+        if self.prev_status == curr_status:
             return
-        self.last_button_status = GPIO.input(self.sw_pin)
-        if GPIO.input(self.sw_pin):
-            #micropython.schedule(self.call_handlers, Rotary.SW_RELEASE)
-            self.call_handlers(Rotary.SW_RELEASE)
-        else:
-            #micropython.schedule(self.call_handlers, Rotary.SW_PRESS)
-            self.call_handlers(Rotary.SW_PRESS)
-            
-    def add_handler(self, handler):
-        self.handlers.append(handler)
-    
-    def call_handlers(self, type):
-        for handler in self.handlers:
-            handler(type)
-            
-            
-            
-            
-            
+        
+        # 4 bits
+        transition = self.prev_status << 2 | curr_status
+        
+        # clockwise
+        if transition == 0b1110:
+            # add callback
+        
+        # counter-clockwise
+        if transition == 0b1101:
+            # add callback
+
+        self.prev_status = curr_status
+
+    def sw_change(channel):
+        print("sw_change")
