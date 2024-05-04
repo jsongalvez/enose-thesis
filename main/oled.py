a4f6d310b0d6357983602f34169cdee576274240
jsongalvez <jsgalvez@mymail.mapua.edu.ph>
2024-05-04 17:20:10 +0800

    saving

--- main/oled.py
+++ main/oled.py
@@ -1,4 +1,5 @@
 import time
+from rotary import Rotary
 
 # For OLED
 from luma.core.interface.serial import spi
@@ -14,31 +15,51 @@ font_size = 24
 anchor_start = 20 
 anchor_buffer = 20
 
+# Oled Setup
+
+
+
+# Rotary setup
+rotary = Rotary(20,8,2,7,21) 
+
+def rotary_changed(event):
+    global current_index
+    if event == Rotary.ROT_CW and current_index < len(menu_items) - 1:
+        current_index += 1
+        draw_menu(draw, font)
+        device.clear()
+        device.display(image)
+    if event == Rotary.ROT_CCW and current_index > 0:
+        current_index -= 1
+        draw_menu(draw, font)
+        device.clear()
+        device.display(image)
+
+
 def draw_menu(draw, font):
     for i, item in enumerate(menu_items):
-        bbox = draw.textbbox((0, anchor_start), f"{item}", font=font)
+        bbox = draw.textbbox((0, 0), f"{item}", font=font)
         x0, y0, x1, y1 = bbox
+        text_height = y1 - y0
         if i == current_index:
             #draw.rectangle((0, anchor_start + i * anchor_buffer, 160, 45 + i * 10), fill="black", outline="black")
-            draw.rectangle((x0, y0, device.width, y1), fill="black", outline="black")
-            draw.text((0, anchor_start + i * anchor_buffer), f"{item}", fill="white", font=font)
+            draw.rectangle((x0, y0 + i * text_height , device.width, y1 + i * text_height), fill="white", outline="white")
+            draw.text((x0, y0 + i * text_height -7), f"{item}", fill="black", font=font)
+            print(bbox)
         else:
-            draw.text((0, anchor_start + i * anchor_buffer), f"{item}", fill="black", font=font)
+            draw.text((x0, y0 + i * text_height -7), f"{item}", fill="white", font=font)
+            print(bbox)
 
-def menu():
-    font_path = Path(__file__).resolve().parent.joinpath('fonts','code2000.ttf')
-    font = ImageFont.truetype(font_path, font_size)
-
-    image = Image.new("RGB", device.size, "white")
-    draw = ImageDraw.Draw(image)
+def main():
 
     draw_menu(draw, font)
 
     device.display(image)
 
-    time.sleep(3)
+    rotary.add_handler(rotary_changed)
 
-    device.clear()
+
+
     while True:
         pass
     
@@ -46,7 +67,14 @@ def menu():
 if __name__ == "__main__":
     try:
         serial = spi(port=0, device=0, gpio_DC=23, gpio_RST=24)
-        device = st7735(serial, rotate=0, h_offset=1, inverse=True)
-        menu()
+        device = st7735(serial, rotate=0, h_offset=1, v_offset=27, inverse=True)
+
+        font_path = Path(__file__).resolve().parent.joinpath('fonts','code2000.ttf')
+        font = ImageFont.truetype(font_path, font_size)
+
+        image = Image.new("RGB", device.size, "black")
+        draw = ImageDraw.Draw(image)
+
+        main()
     except KeyboardInterrupt:
         pass
